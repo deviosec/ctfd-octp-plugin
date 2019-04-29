8ad09ee20708543acbc6d8ad460c67d5c7ed7674
eyjhbb@gmail.com <eyjhbb@gmail.com>
2019-04-29 21:22:50 +0200

    added basic options, currently nothing works!

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 # stuff to mkae this work
-from CTFd.plugins import register_user_page_menu_bar
+from CTFd.plugins import register_user_page_menu_bar, register_admin_plugin_menu_bar
 from CTFd.plugins import register_plugin_assets_directory
 from CTFd.utils.plugins import register_script, register_stylesheet
 from CTFd.utils.user import get_current_user
 from CTFd.utils.decorators import authed_only
 from CTFd.utils.user import get_current_user, get_current_team, is_admin, authed
+from CTFd.utils import get_config, set_config
 
 # get our path
 import os
@@ -21,9 +22,14 @@ from CTFd.utils.plugins import override_template
 from CTFd.models import db
 import functools
 
-
 import requests
 import json
+
+class Error(Exception):
+    pass
+
+class CtffOctpNoUrl(Exception):
+    pass
 
 def octp_require_auth(f):
     """
@@ -45,13 +51,45 @@ def octp_require_auth(f):
 
     return authed_only_wrapper
 
+def octp_require_admin(f):
+    """
+    Decorator that requires the user to be authenticated
+    :param f:
+    :return:
+    """
+
+    @functools.wraps(f)
+    def authed_only_wrapper(*args, **kwargs):
+        if is_admin():
+            return f(*args, **kwargs)
+        else:
+            if request.content_type == 'application/json':
+                abort(403)
+            else:
+                # return redirect(url_for('auth.login', next=request.full_path))
+                return render_template('page.html', content="You need to be admin to access this page!")
+
+    return authed_only_wrapper
+
 class ctfdoctp(object):
     def __init__(self, app):
         # get our app handle
         self.app = app
 
+        # make our default config, if needed
+        self.initialConfig()
+
+        set_config("octp_enable", True)
+        set_config("octp_show_frontends", False)
+        set_config("octp_show_intercept", False)
+        set_config("octp_url", "http://127.0.0.1:8000/")
+        if not get_config("octp_enable"):
+            return
+
         # init our OCTP api
-        self.octp_api = octp.Octp("http://127.0.0.1:8000")
+        if not get_config("octp_url"):
+            raise CtffOctpNoUrl("No backend URL configured")
+        self.octp_api = octp.Octp(get_config("octp_url"))
 
         # find our plugin path!
         self.fullPath = os.path.dirname(os.path.realpath(__file__))+"/"
@@ -63,10 +101,11 @@ class ctfdoctp(object):
         self.addTemplate('octp-labcentral.html', 'labcentral.html')
         self.addTemplate('octp-lab.html',        'lab.html')
         self.addTemplate('octp-frontend.html',   'frontend.html')
+        self.addTemplate('octp-admin-settings.html',   'admin/index.html')
+        self.addTemplate('octp-admin-incl-settings.html',   'admin/settings.html')
 
         # register our assets
         register_plugin_assets_directory(app, base_path=self.partialPath+"assets/")
-        print(self.partialPath+"assets/")
         register_stylesheet(self.partialPath+"assets/style.css")
         register_script(self.partialPath+"assets/axios.min.js")
         register_script(self.partialPath+"assets/script.js")
@@ -79,22 +118,48 @@ class ctfdoctp(object):
         # register our menu bar!
         register_user_page_menu_bar("Lab Central", "/octp/labcentral")
 
+        # register our admin menu bar
+        register_admin_plugin_menu_bar("OCTP", "/octp/admin/settings")
+
         # add our routes
         ## our basic routes
         app.add_url_rule("/octp/labcentral", methods=['GET'], view_func=self.labCentral)
-        app.add_url_rule("/octp/labinfo", methods=['GET'], view_func=self.getLabInformtion)
-        app.add_url_rule("/octp/frontendinfo", methods=['GET'], view_func=self.getFrontendInformation)
-        app.add_url_rule("/octp/interceptinfo", methods=['GET'], view_func=self.getInterceptInformation)
+        app.add_url_rule("/octp/admin/settings", methods=['GET'], view_func=self.adminSettings)
 
-        ## our api routes
-        app.add_url_rule("/octp/claimlab", methods=['GET'], view_func=self.getClaimLab)
-        app.add_url_rule("/octp/claimfrontend", methods=['GET'], view_func=self.getClaimFrontend)
+        if get_config("octp_show_labs"):
+            app.add_url_rule("/octp/labinfo", methods=['GET'], view_func=self.getLabInformtion)
+            app.add_url_rule("/octp/claimlab", methods=['GET'], view_func=self.getClaimLab)
+
+        if get_config("octp_show_frontends"):
+            app.add_url_rule("/octp/frontendinfo", methods=['GET'], view_func=self.getFrontendInformation)
+            app.add_url_rule("/octp/claimfrontend", methods=['GET'], view_func=self.getClaimFrontend)
+
+        if get_config("octp_show_intercept"):
+            app.add_url_rule("/octp/interceptinfo", methods=['GET'], view_func=self.getInterceptInformation)
+
+    # creates our default configuration, which decides what we sill display, etc.
+    def initialConfig(self):
+        if not get_config("octp_enable"):
+            set_config("octp_enable", os.environ.get("OCTP_ENABLE", True))
+        if not get_config("octp_url"):
+            set_config("octp_url", os.environ.get("OCTP_URL", ""))
+        if not get_config("octp_show_labs"):
+            set_config("octp_show_labs", os.environ.get("OCTP_SHOW_LABS", True))
+        if not get_config("octp_show_frontends"):
+            set_config("octp_show_frontends", os.environ.get("OCTP_SHOW_FRONTENDS", True))
+        if not get_config("octp_show_intercept"):
+            set_config("octp_show_intercept", os.environ.get("OCTP_SHOW_intercept", True))
+
 
     def noVnc(self):
         return render_template('octp-novnc.html', basepath=self.partialPath+"novnc")
 
     def addTemplate(self, name, path):
         override_template(name, open(self.fullPath+"/templates/"+path).read())
+
+    @octp_require_admin
+    def adminSettings(self):
+        return render_template('octp-admin-settings.html')
 
     @octp_require_auth
     def labCentral(self):
@@ -184,7 +249,23 @@ class ctfdoctp(object):
         return json.dumps({"error": ""})
 
 # lets have a table to hold our data
-class OctpModel(db.Model):
+class OctpRelations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # team = db.Column(db.Integer, db.ForeignKey('teams.id'))
+    labId = db.Column(db.Text)
+    labIp = db.Column(db.Text)
+    frontendId = db.Column(db.Text)
+    frontendIp = db.Column(db.Text)
+
+    def __init__(self, user, labId, labIp, frontendId, frontendIp):
+        self.user = user
+        self.labId = labId
+        self.labIp = labIp
+        self.frontendId = frontendId
+        self.frontendIp = frontendIp
+
+class OctpRelations2(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Integer, db.ForeignKey('users.id'))
     # team = db.Column(db.Integer, db.ForeignKey('teams.id'))
